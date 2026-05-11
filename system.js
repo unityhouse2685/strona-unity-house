dfe2655ac7aacee77dbc6890738732b46058a767
unityhouse2685 <unity.housegostyn@gmail.com>
2026-05-11 19:30:07 +0200

    Update system.js

--- system.js
+++ system.js
@@ -2,7 +2,7 @@
    KONFIGURACJA SUPABASE
 ============================================================ */
 
-const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
+const SUPABASE_URL = "https://ycuogutnwdybdeobowla.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljdW9ndXRud2R5YmRlb2Jvd2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTE4NDAsImV4cCI6MjA5NDA2Nzg0MH0.ObkFIknc3Ce5KEmj435lI_8hi1T7E-lnxQuRSicZlPw";
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
